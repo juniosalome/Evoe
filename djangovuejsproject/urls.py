60a770141e0bb10ecdf58f39ebda1ccbaa64f1b3
juniosalome <junio.salome@gmail.com>
2021-05-04 22:47:03 -0300

    Update urls.py

--- djangovuejsproject/urls.py
+++ djangovuejsproject/urls.py
@@ -19,7 +19,7 @@ from django.urls import path
 from django.conf.urls import url
 from django.views.generic import TemplateView
 from django.views.generic import RedirectView
-from catalog import views
+from catalogo import views
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
