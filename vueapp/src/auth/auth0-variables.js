c2791ecd9969a8c939c73bb301b3d299fc12cea1
juniosalome <junio.salome@gmail.com>
2021-05-06 10:40:19 -0300

    Update auth0-variables.js

--- vueapp/src/auth/auth0-variables.js
+++ vueapp/src/auth/auth0-variables.js
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
-  clientId: '89W9UuJx0KvoNSVzCuonXxRvAdT1ZoPm',
-  domain: 'juniosalome.us.auth0.com',
+  clientId: 'TcHzYEyEzpJ0gYtYVyavXXjidRjQ7Yqw',
+  domain: 'techiediaries.auth0.com',
   callbackUrl: 'http://localhost:8000/callback',
-  apiUrl: 'https://juniosalome.us.auth0.com/api/v2/'
+  apiUrl: 'https://djangovuedemo.techiediaries.com'
 }
